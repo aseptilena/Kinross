f22021f275b0d7c1834ada1034f8755d2336f34b
Parcly Taxel <reddeloostw@gmail.com>
2015-04-28 00:01:56 +0800

    Inkscape extensions, yay!

---
+++ ellipsefocus/ellipsefocus.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+from __future__ import division
+import sys, inkex, gettext, math
+sys.path.append('/usr/share/inkscape/extensions')
+def pout(t): sys.exit((gettext.gettext(t)))
+
+class root(inkex.Effect):
+    def __init__(self):
+        inkex.Effect.__init__(self)
+
+    def effect(self):
+        if len(self.selected) == 0: pout("Please select at least one ellipse.")
+        for obj in self.selected:
+            curr = self.selected[obj]
+            x, y, a, b = float(curr.get("cx")), float(curr.get("cy")), float(curr.get("rx")), float(curr.get("ry"))
+            d = math.sqrt(max(a, b) ** 2 - min(a, b) ** 2)
+            dstring = "M{0} {1}{2}{3}".format(x - d * (a >= b), y - d * (a <= b), 'h' if a > b else 'v', 2 * d)
+            attribs = {'style': curr.get("style"), 'd': dstring, 'transform': curr.get("transform")}
+            inkex.etree.SubElement(curr.getparent(), inkex.addNS('path','svg'), attribs)
+
+final = root()
+final.affect()

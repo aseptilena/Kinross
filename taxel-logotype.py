a0c609adcd1e904593a36531148b31b5e2ee92e2
Parcly Taxel <reddeloostw@gmail.com>
2015-04-27 12:18:58 +0800

    Revert! Revert! It's 27/128 and always will be!

--- taxel-logotype.py
+++ taxel-logotype.py
@@ -10,4 +10,4 @@ t = (69 / (4 * (1 + m * m))).sqrt()
 p, q = (l - k) / 2 - t, (l + k) / 2 - m * t
 d = [p, -q, l, -l, q, -p]
 c = [str(round(i * (Decimal(2) / Decimal(3)).sqrt() + 4, g // 2)) for i in d]
-with open("coords.svg", 'w') as out: out.write('<svg><rect fill="#230f38" width="8" height="8"/><path style="fill:none;stroke:#6dc6fb;stroke-width:.25;stroke-linecap:round;stroke-linejoin:round" d="M1 1 {0} 7 7"/><svg>'.format(" ".join(c)))
+with open("coords.svg", 'w') as out: out.write('<svg><rect fill="#230f38" width="8" height="8"/><path style="fill:none;stroke:#6dc6fb;stroke-width:.2109375;stroke-linecap:round;stroke-linejoin:round" d="M1 1 {0} 7 7"/><svg>'.format(" ".join(c)))

61c140fcd42ff978df5a3edc46f307dfc018466a
Parcly Taxel <reddeloostw@gmail.com>
2015-08-08 20:22:17 +0800

    More point functions

--- utilities/d0.py
+++ utilities/d0.py
@@ -4,7 +4,11 @@
 # http://parclytaxel.tumblr.com
 from math import *
 
-# A point in the plane, equivalently a complex number.
+# Proximal function
+def prox(a, b = 0, e = 1e-5):
+    return a - b <= e and a - b >= -e
+
+# A point in the plane, equivalently a vector from the origin or complex number.
 # For trigonometric operations, positive angle is clockwise to follow SVG convention.
 class Point:
     def __init__(self): # Origin
@@ -20,19 +24,37 @@ class Point:
     def __sub__(self, that): return Point(self.x - that.x, self.y - that.y)
     def __neg__(self): return node(-self.x, -self.y)
     
-    def __mul__(self, k): return Point(self.x * k, self.y * k) # Scalar multiplication
+    def __mul__(self, that):
+        if type(that) == Point: return Point(self.x * that.x - self.y * that.y, self.y * that.x + self.x * that.y)
+        else: return Point(self.x * k, self.y * k)
     def __rmul__(self, k): return self * k
-    def __truediv__(self, k): return Point(self.x / k, self.y / k) # Scalar division
+    def __truediv__(self, k):
+        if type(that) == Point:
+            n = that.x * that.x + that.y * that.y
+            return Point(self.x * that.x + self.y * that.y, self.y * that.x - self.x * that.y) / n
+        else: return Point(self.x / k, self.y / k)
+    
+    def is0(): return prox(self.x) and prox(self.y)
     # Modulus and argument
     def md(): return hypot(self.x, self.y)
+    def md2(): return self.x * self.x + self.y * self.y
     def th(): return atan2(self.y, self.x)
+    # Classic vector operations
+    def hat(): return self / md(self) if !self.is0() else Point() # The normalised vector, denoted with a hat
+    def rot(th):
+        s, c = sin(th), cos(th)
+        return Point(self.x * c - self.y * s, self.x * s + self.y * c)
+    def lturn(): return Point(self.y, -self.x) # Both of these
+    def rturn(): return Point(-self.y, self.x) # rotate by 90 degrees
 
 def pplf(md, th): return Point(md * cos(th), md * sin(th)) # Point from polar form (argument in radians of course)
-# Complex multiplication and division
-def cpmult(p1, p2): return Point(p1.x * p2.x - p1.y * p2.y, p1.y * p2.x + p1.x * p2.y)
-def cpdiv(p1, p2):
-    n = p2.x * p2.x + p2.y * p2.y
-    return Point(p1.x * p2.x + p1.y * p2.y, p1.y * p2.x - p1.x * p2.y) / n
-# Functions of the first point... if the second is the origin
+def pplf1(th): return Point(cos(th), sin(th)) # The same, but returning a unit vector
+
+# Relative functions (origin is stated)
 def md(p, origin): return md(p - origin)
 def th(p, origin): return th(p - origin)
+
+# TODO dot product
+
+# Linear interpolation
+def slide(p, q, t): return p + t * (q - p)

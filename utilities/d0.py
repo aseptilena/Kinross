cab27e7cc0485e7362939615d4bb876bd57ca0d4
Parcly Taxel <reddeloostw@gmail.com>
2015-08-09 01:20:02 +0800

    Preparation for the Rytz construction

--- utilities/d0.py
+++ utilities/d0.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3.4
-# Helper functions defining all the flavours of Kinross
+# Helper functions for Kinross: vector geometry
 # Parcly Taxel / Jeremy Tan, 2015
 # http://parclytaxel.tumblr.com
-from math import *
+from math import hypot, atan2, sin, cos
 
-# Proximal function
-def prox(a, b = 0, e = 1e-5):
-    return a - b <= e and a - b >= -e
+def prox(a, b = 0, e = 1e-5): return a - b <= e and a - b >= -e # Proximal function
 
 # A point in the plane, equivalently a vector from the origin or complex number.
 # For trigonometric operations, positive angle is clockwise to follow SVG convention.
@@ -26,35 +24,41 @@ class Point:
     
     def __mul__(self, that):
         if type(that) == Point: return Point(self.x * that.x - self.y * that.y, self.y * that.x + self.x * that.y)
-        else: return Point(self.x * k, self.y * k)
-    def __rmul__(self, k): return self * k
-    def __truediv__(self, k):
+        else: return Point(self.x * that, self.y * that)
+    def __rmul__(self, that): return self * that
+    def __truediv__(self, that):
         if type(that) == Point:
             n = that.x * that.x + that.y * that.y
             return Point(self.x * that.x + self.y * that.y, self.y * that.x - self.x * that.y) / n
-        else: return Point(self.x / k, self.y / k)
+        else: return Point(self.x / that, self.y / that)
     
-    def is0(): return prox(self.x) and prox(self.y)
+    def is0(self): return prox(self.x) and prox(self.y)
     # Modulus and argument
-    def md(): return hypot(self.x, self.y)
-    def md2(): return self.x * self.x + self.y * self.y
-    def th(): return atan2(self.y, self.x)
+    def md(self): return hypot(self.x, self.y)
+    def md2(self): return self.x * self.x + self.y * self.y
+    def th(self): return atan2(self.y, self.x)
     # Classic vector operations
-    def hat(): return self / md(self) if !self.is0() else Point() # The normalised vector, denoted with a hat
-    def rot(th):
+    def hat(self): return self / self.md() if not self.is0() else Point(1, 0) # The normalised vector, denoted with a hat
+    def lenbyhat(self, l): return self.hat() * l # length * hat(vector)
+    def rot(self, th):
         s, c = sin(th), cos(th)
         return Point(self.x * c - self.y * s, self.x * s + self.y * c)
-    def lturn(): return Point(self.y, -self.x) # Both of these
-    def rturn(): return Point(-self.y, self.x) # rotate by 90 degrees
+    def lturn(self): return Point(self.y, -self.x) # Both rotate by 90 degrees
+    def rturn(self): return Point(-self.y, self.x)
 
-def pplf(md, th): return Point(md * cos(th), md * sin(th)) # Point from polar form (argument in radians of course)
-def pplf1(th): return Point(cos(th), sin(th)) # The same, but returning a unit vector
-
-# Relative functions (origin is stated)
-def md(p, origin): return md(p - origin)
-def th(p, origin): return th(p - origin)
+# For each of the methods in Point above there is a corresponding relative function taking a given point as the origin.
+# Some names change to reflect this distinction.
+def pnear(p, origin): return (p - origin).is0()
+def dist(p, origin): return (p - origin).md()
+def dirc(p, origin): return (p - origin).th()
+def hat(p, origin): return (p - origin).hat()
+def lenbyhat(p, origin, l): return (p - origin).lenbyhat(l)
+def rot(p, origin, th): return (p - origin).rot(th)
+def lturn(p, origin): return (p - origin).lturn()
+def rturn(p, origin): return (p - origin).rturn()
 
 # TODO dot product
 
-# Linear interpolation
+# Functions between two points
+def midpoint(p, q): return (p + q) / 2
 def slide(p, q, t): return p + t * (q - p)
---
+++ utilities/ellipse.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3.4
+# Helper functions for Kinross: ellipses
+# Parcly Taxel / Jeremy Tan, 2015
+# http://parclytaxel.tumblr.com
+from d0 import *
+
+# OY OY OY

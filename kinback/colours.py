953c05dfd857654d5f66bada956fda9bc47408d0
Parcly Taxel <reddeloostw@gmail.com>
2015-08-14 10:58:08 +0800

    CIEXYZ 1931, CIELAB 1976, PARCLY 2015

--- kinback/colours.py
+++ kinback/colours.py
@@ -166,11 +166,36 @@ def fromccol(c):
 
 # Because I'm a completeness freak, conversions to other colour spaces.
 # r = sRGB, x = CIEXYZ (1931), l = CIELAB (1976)
+# Tristimulus values for D65:
+xn, yn, zn = .95047, 1., 1.08883
 def r2x(c):
     def linearise(k): return k / 12.92 if k <= .04045 else ((k + .055) / 1.055) ** 2.4
-    cc = (linearise(k) for k in c[:3])
+    cc = [linearise(k) for k in c[:3]]
     z = [.4124 * cc[0] + .3576 * cc[1] + .1805 * cc[2],
          .2126 * cc[0] + .7152 * cc[1] + .0722 * cc[2],
          .0193 * cc[0] + .1192 * cc[1] + .9505 * cc[2]]
+    if len(c) == 4: z.append(c[3])
+    return tuple(z)
+def x2r(c):
+    def delinearise(k): return 12.92 * k if k <= .0031308 else 1.055 * k ** (1 / 2.4) - 0.055
+    cc = [3.2406 * c[0] - 1.5372 * c[1] -  .4986 * c[2],
+          -.9689 * c[0] + 1.8758 * c[1] +  .0415 * c[2],
+           .0557 * c[0] -  .2040 * c[1] + 1.0570 * c[2]]
+    z = [delinearise(k) for k in cc]
+    if len(c) == 4: z.append(c[3])
+    return tuple(z)
+def x2l(c):
+    def cise(k): return k ** (1 / 3) if k > 216 / 24389 else k * 841 / 108 + 4 / 29
+    z = [116 * cise(c[1] / yn) - 16,
+         500 * (cise(c[0] / xn) - cise(c[1] / yn)),
+         200 * (cise(c[1] / yn) - cise(c[2] / zn))]
+    if len(c) == 4: z.append(c[3])
+    return tuple(z)
+def l2x(c):
+    def icise(k): return k ** 3 if k > 6 / 29 else 108 / 841 * (k - 4 / 29)
+    l0 = (c[0] + 16) / 116
+    z = [xn * icise(l0 + c[1] / 500),
+         yn * icise(l0),
+         zn * icise(l0 - c[2] / 200)]
     if len(c) == 4: z.append(c[3])
     return tuple(z)

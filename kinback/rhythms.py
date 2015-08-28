c64ad3c461944dbaba80bee57a6e10c9e24023a2
Parcly Taxel <reddeloostw@gmail.com>
2015-08-28 10:07:36 +0800

    Derpy has just grown to a large size

---
+++ kinback/rhythms.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3.4
+# Helper functions for Kinross: rhythms (SVG path processing)
+# Parcly Taxel / Jeremy Tan, 2015
+# http://parclytaxel.tumblr.com
+
+# TODO as in the readme
+def parserhythm(p):
+    pass
+
+def outputrhythm(k):
+    pass

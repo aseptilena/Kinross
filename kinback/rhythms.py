7a00e10424370917fde6940dca584e7985f9289a
Parcly Taxel <reddeloostw@gmail.com>
2015-08-28 15:15:29 +0800

    Take seven numbers and move on

--- kinback/rhythms.py
+++ kinback/rhythms.py
@@ -2,10 +2,40 @@
 # Helper functions for Kinross: rhythms (SVG path processing)
 # Parcly Taxel / Jeremy Tan, 2015
 # http://parclytaxel.tumblr.com
+import re
+from .vectors import * # local
+# There is pretty much only one way to write the real number regex; accordingly this is spliced from Scour
+number = re.compile(r"([-+]?(?:(?:[0-9]*\.[0-9]+)|(?:[0-9]+\.?))(?:[eE][-+]?[0-9]+)?)")
+spacing = re.compile(r"[ ,]*")
 
 # TODO as in the readme
+# Converts all coordinates to absolute first
 def parserhythm(p):
-    pass
+    # pos should only stop on letters or the start of an implicit rhythm
+    out, pos, cursor = [], 0, point(0., 0.)
+    take, prevailing, params = 2, "M", []
+    t, tokens = [], [c for c in number.split(p) if not spacing.fullmatch(c)]
+    for v in tokens:
+        if " " in v or v.isalpha(): t.extend(v.replace(" ", ""))
+        else: t.append(float(v))
+    print(t)
+    while pos < len(t):
+        # Two phases. First obtain the command and its parameters...
+        val = t[pos]
+        if type(val) == str:
+            if val in "Cc": take = 6
+            elif val in "MmLlTt": take = 2
+            elif val in "Zz": take = 0
+            elif val in "HhVv": take = 1
+            elif val in "SsQq": take = 4
+            elif val in "Aa": take = 7
+            prevailing, params = val, t[pos + 1:pos + take + 1]
+            pos += take + 1
+        else:
+            params = t[pos:pos + take]
+            pos += take
+        # ...then update out TODO
+        print(params)
 
-def outputrhythm(k):
-    pass
+# def outputrhythm(k):
+#     pass

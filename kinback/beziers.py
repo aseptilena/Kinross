77d1f208413cf6b381e8b17c23ed0aadb025e36f
Parcly Taxel <reddeloostw@gmail.com>
2015-09-25 10:51:12 +0800

    Bézier class, numerical processes incoming

---
+++ kinback/beziers.py
@@ -0,0 +1,22 @@
+# Helper functions for Kinross: linear, quadratic and cubic Bezier curves
+# Parcly Taxel / Jeremy Tan, 2015
+# http://parclytaxel.tumblr.com
+from .vectors import *
+
+class bezier:
+    def __init__(self, *p):
+        self.p = list(p)[:min(4, len(p))] # cull to cubic curves, since they are the highest degree used in SVG
+        self.deg = len(self.p) - 1
+    def __str__(self): return "<{}>".format(", ".join([printpoint(n) for n in self.p]))
+    def __repr__(self): return "bezier({})".format(", ".join([str(n) for n in self.p]))
+    
+    def __call__(self, t):
+        q = self.p[:]
+        while len(q) > 1: q = [linterp(q[i], q[i + 1], t) for i in range(len(q) - 1)]
+        return q[0]
+    def start(self): return self.p[0]
+    def end(self): return self.p[-1]
+    
+    def d(self):
+        """The derivative of this Bezier curve."""
+        return bezier(*[self.deg * (self.p[i + 1] - self.p[i]) for i in range(self.deg)])

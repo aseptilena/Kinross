14154da83d26e09b49f83d022ec663620d38fb91
Parcly Taxel <reddeloostw@gmail.com>
2015-10-11 13:51:00 +0800

    The straight stars have been assimilated

--- standalones/spallator.py
+++ standalones/spallator.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3.5
 # "Spallates" an input object on a canvas, generating a random pattern according to given variability data.
+# (This is not Splatoon, mind you, it's the nuclear process)
 # The circle and star templates included match the behaviour of the old circle-starfield and star-starfield scripts,
 # being used to generate MLPFIM starry sky backgrounds and magic sparkles / Luna's mane stars respectively.
 # This script consolidates them both and was written for generating Nightmare Rarity's mane stars, which are astroids.
@@ -7,12 +8,13 @@
 # http://parclytaxel.tumblr.com
 import random, sys
 rgen = random.SystemRandom()
-from math import log
+from math import log, pi
 from kinback.miscellanea import xmlprettyprint, svgnms
 import xml.etree.ElementTree as t
 for n in svgnms: t.register_namespace(n, svgnms[n])
 sdpns = "{http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd}"
 
+def baseisstar(): return base.tag == "path" and base.get(sdpns + "type") == "star"
 def nextspall():
     dt = base.attrib.copy()
     if distro[0] == 'g':
@@ -24,22 +26,32 @@ def nextspall():
         dt["r"] = str(float(dt["r"]) + float(distro[-1]) * variate)
         dt["cx"], dt["cy"] = str(round(rgen.random() * width, 3)), str(round(rgen.random() * height, 3))
         return t.Element("circle", dt)
+    elif baseisstar():
+        outr, inr = float(dt[sdpns + "r1"]), float(dt[sdpns + "r2"])
+        srat = inr / outr
+        outr += float(distro[-1]) * variate
+        inr = outr * srat
+        dt[sdpns + "r1"], dt[sdpns + "r2"] = str(round(outr, 3)), str(round(inr, 3))
+        if spins:
+            delta = 2 * pi * rgen.random() / int(dt[sdpns + "sides"])
+            dt[sdpns + "arg1"] = str(round(float(dt[sdpns + "arg1"]) + delta, 9))
+            dt[sdpns + "arg2"] = str(round(float(dt[sdpns + "arg2"]) + delta, 9))
+        dt[sdpns + "cx"], dt[sdpns + "cy"] = str(round(rgen.random() * width, 3)), str(round(rgen.random() * height, 3))
+        return t.Element("path", dt)
 
-if len(sys.argv) != 3: # The file should be like <svg height="" width="" ...><object/></svg>, dimensions defaulting to 1000.
+if len(sys.argv) != 3:
      print("Usage: {} [file] [params]\nSee the documentation for details.".format(sys.argv[0]))
      sys.exit(1)
-rn, vard = t.parse(sys.argv[1]).getroot(), sys.argv[2].split(";") # "0.777;g 2.5 4 5;y" matches star-starfield
+rn, vard = t.parse(sys.argv[1]).getroot(), sys.argv[2].split(";")
 base, width, height = rn[0], float(rn.get("width", "1000")), float(rn.get("height", "1000"))
-distro, turn = vard[1].split(), bool(vard[2])
-# The density is a relative measure of how many objects are put into a 1000-by-1000 square, with 250 objects as 1, 500 as 2 and so on.
+distro, spins = vard[1].split(), bool(vard[2])
+# Density is a relative measure of how many objects are put into a 1000-by-1000 square: 250 objects = 1, 500 = 2, etc.
 # This corresponds to the old circle-starfield default behaviour; the aforementioned object count is width * height * ("effective radius") ^ 2 / 49000.
-# The effective radius for circles, ellipses and stars is the average of their two radii.
+# The effective radius for circles and stars is the average of their two radii.
 density = float(vard[0]) if vard[0] else 1
 if base.tag == "circle": er = float(base.get("r"))
-elif base.tag == "ellipse": er = (float(base.get("rx")) + float(base.get("ry"))) / 2
-elif base.tag == "path" and base.get(sdpns + "type") == "star": er = (float(base.get(sdpns + "r1")) + float(base.get(sdpns + "r2"))) / 2
+elif baseisstar(): er = (float(base.get(sdpns + "r1")) + float(base.get(sdpns + "r2"))) / 2
 N = round(width * height * er * er * density / 49000)
-
 try: gparam = log(float(distro[1]))
 except ValueError: gparam = 1.0986123 # equivalent to a decay rate of 3
 

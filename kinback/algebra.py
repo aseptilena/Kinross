cbdfc4648708d9c62903b81ac7aecd7eefad1bca
Parcly Taxel <reddeloostw@gmail.com>
2015-12-16 18:32:38 +0800

    Ramshacking of polynomial class to use floats

--- kinback/algebra.py
+++ kinback/algebra.py
@@ -1,146 +1,119 @@
 # Helper functions for Kinross: numerical algebra and methods
 # Parcly Taxel / Jeremy Tan, 2015
 # http://parclytaxel.tumblr.com
+from math import sqrt, hypot
 from cmath import isclose
 from itertools import product
 from decimal import Decimal as D, getcontext
+import time
 getcontext().prec = 60
 zero = D(0)
 
-class polynomial:
-    """A polynomial stores a list of coefficients [a0, a1, a2, ...] where a0 is the constant term, a1 is the x term and so on."""
-    def __init__(self, coeffs = 0):
-        if type(coeffs) in (int, float, D): self.a = [D(coeffs)]
-        else: self.a = [D(c) for c in coeffs]
+class polyn:
+    """A polyn(omial) stores a list of (real) coefficients [a0, a1, a2, ...] where a0 is the constant term, a1 is the x term and so on."""
+    def __init__(self, *coeffs): self.a = list(coeffs)
     def __getitem__(self, n): return self.a[n] # The coefficient associated with the nth power; leading coefficient is self[-1]
-    def __setitem__(self, n, v): self.a[n] = D(v)
+    def __setitem__(self, n, v): self.a[n] = v
     def __len__(self): return len(self.a)
     def deg(self): return len(self) - 1
-    def powers(self):
-        """An iterator over the powers of the polynomial, counting towards the constant term."""
-        return range(len(self) - 1, -1, -1)
-    def __str__(self): return " + ".join([str(self.a[i]) + "*x^" + str(i) for i in self.powers()])
-    def __repr__(self): return "polynomial([" + ",".join([repr(c) for c in self.a]) + "])"
-    def __call__(self, x):
-        """The Horner scheme for polynomial evaluation. Since this is what polynomials are all about it seemed natural to assign this method as such."""
-        res = zero
-        for i in self.powers(): res = res.fma(x, self[i])
+    def powers(self): return range(len(self) - 1, -1, -1) # High-to-low iterator over the polynomial's powers
+    def __str__(self): return " + ".join(["{}*x^{}".format(self[i], i) for i in self.powers()])
+    def __repr__(self): return "polyn(" + ", ".join([str(c) for c in self.a]) + ")"
+    def __call__(self, x): # Horner scheme
+        res = 0
+        for i in self.powers(): res = res * x + self[i]
         return res
     def dup(self):
         """Returns an independent duplicate of the polynomial."""
-        return polynomial(self.a)
+        return polyn(self.a)
+    
     def __add__(self, q):
-        res = self.a + [zero] * max(0, len(q) - len(self))
+        res = self.a + [0] * max(0, len(q) - len(self))
         for i in range(len(q)): res[i] += q[i]
-        return polynomial(res)
+        return polyn(*res)
     def __sub__(self, q):
-        res = self.a + [zero] * max(0, len(q) - len(self))
+        res = self.a + [0] * max(0, len(q) - len(self))
         for i in range(len(q)): res[i] -= q[i]
-        return polynomial(res)
-    def __neg__(self): return polynomial([-c for c in self.a])
-    def addzeroroots(self, n = 1):
-        """The polynomial is multiplied by x^n, thereby introducing n roots of zero."""
-        return polynomial([0] * n + self.a)
+        return polyn(*res)
+    def __neg__(self): return polyn(*[-c for c in self.a])
     def __mul__(self, q):
-        if type(q) != polynomial: return polynomial([c * D(q) for c in self.a])
-        res = [zero] * (len(self) + len(q) - 1)
+        if type(q) != polyn: return polyn(*[c * q for c in self.a])
+        res = [0] * (len(self) + len(q) - 1)
         for p in product(range(len(self)), range(len(q))): res[sum(p)] += self[p[0]] * q[p[1]]
-        return polynomial(res)
+        return polyn(*res)
     def __rmul__(self, q): return self * q
-    def __divmod__(self, b): # Euclidean algorithm for polynomials
-        q, r, d, c = polynomial(), self.dup(), b.deg(), b[-1]
-        while r.deg() >= d:
-            s = polynomial(r[-1] / c).addzeroroots(r.deg() - d)
-            q += s
-            r -= s * b
-            r.a.pop()
-        return (q, r)
+    def __divmod__(self, b):
+        if type(b) != polyn: return (polyn(*[c / b for c in self.a]), polyn(0))
+        r, d, q = self.a[:], b.a, [] # d is the divisor
+        while len(r) >= len(d):
+            q.append(r[-1] / d[-1])
+            for i in range(-1, -len(d) - 1, -1): r[i] -= q[-1] * d[i]
+            r.pop()
+        return (polyn(*q[::-1]), polyn(*r))
     def __floordiv__(self, b): return divmod(self, b)[0]
-    def __mod__(self, b): return divmod(self, b)[1]
     def __truediv__(self, b): return self // b
-    def ruffini(self, r):
-        """Divides the polynomial by x - r using the Ruffini scheme, returning the remainder in a tuple."""
-        res = []
-        for i in self.powers():
-            if i == self.deg(): res.append(self[i])
-            else: res.insert(0, res[0].fma(r, self[i]))
-        return (polynomial(res[1:]), res[0])
+    def __mod__(self, b): return divmod(self, b)[1]
+    def deriv(self): return polyn(*[i * self[i] for i in range(1, len(self))])
+    def antideriv(self): return polyn(*([0] + [self[i] / (i + 1) for i in range(len(self))]))
+    
     def quadruffini(self, u, v):
-        """Divides the polynomial by x^2 + ux + v with a Ruffini-like scheme, returning a tuple (quotient, c, d) where the remainder is cx + d."""
-        q = [zero, zero]
+        """Divides the polynomial by x^2 + ux + v with a Ruffini-like scheme, returning (quotient, c, d) where the remainder is cx + d."""
+        q = [0, 0]
         for i in range(self.deg(), 1, -1): q.append(self[i] - u * q[-1] - v * q[-2])
-        return (polynomial(q[:1:-1]), self[1] - u * q[-1] - v * q[-2], self[0] - v * q[-1])
-    def deriv(self): return polynomial([i * self[i] for i in range(1, len(self))])
-    def antideriv(self): return polynomial([0.] + [self[i] / (i + 1) for i in range(len(self))])
-    def minusendzeros(self, prec = 1e-10):
-        """Returns the polynomial with leading and trailing zeros to the specified precision stripped along with the number of trailing zeros (or equivalently zero roots)."""
-        p = self.dup()
-        while isclose(p[-1], zero, abs_tol=prec) and len(p) > 1: p.a.pop()
-        N = 0
-        while isclose(p[0], zero, abs_tol=prec) and len(p) > 1:
-            p.a.pop(0)
-            N += 1
-        return (p, N)
+        return (polyn(*q[:1:-1]), self[1] - u * q[-1] - v * q[-2], self[0] - v * q[-1])
     def bairstowstep(self, u, v):
         """Calculates one step of Bairstow's method for the given constants (divisor is x^2 + ux + v) to be added to the current guess."""
         q, c, d = self.quadruffini(u, v)
         g, h = q.quadruffini(u, v)[1:]
-        K, L = -g * v, g.fma(u, -h)
+        K, L = -g * v, g * u - h
         frac = g * K + h * L
-        x, y = (d * g - c * h) / frac, (c * K + d * L) / frac
-        return (x, y)
-    def roots(self, prec = 1e-10):
-        """Finds all roots to the specified precision, returning the [[real roots] and [complex roots as [real part, complex part]]].
-        Cubic and higher polynomials are handled by Bairstow's method; in practice this function would be used as part of polynomroot()."""
-        p, nz = self.minusendzeros(prec)
-        if p.deg() == 0: res = [[], []]
-        elif p.deg() == 1: res = [[-p[0] / p[1]], []]
-        elif p.deg() == 2:
-            # See https://people.csail.mit.edu/bkph/articles/Quadratics.pdf for the derivation.
+        return ((d * g - c * h) / frac, (c * K + d * L) / frac)
+    def roots(self):
+        """Finds all ([real], [complex]) roots of the polynomial; cubic and higher degrees are handled by Bairstow's method."""
+        cfs, out = self.a[:], ([], [])
+        while isclose(cfs[-1], 0, abs_tol=1e-15) and len(cfs) > 1: cfs.pop() # scrub high and low zeros to make things easier
+        while isclose(cfs[0], 0, abs_tol=1e-15) and len(cfs) > 1:
+            cfs.pop(0)
+            out[0].append(0)
+        p = polyn(*cfs)
+        if p.deg() == 0: pass
+        elif p.deg() == 1: out[0].append(-p[0] / p[1])
+        elif p.deg() == 2: # derivation from https://people.csail.mit.edu/bkph/articles/Quadratics.pdf
             a, b, c = p[2], p[1], p[0]
             d = b * b - 4 * a * c
-            e = abs(d).sqrt()
+            e = sqrt(abs(d))
             if d < 0:
-                r, i = -b / (2 * a), e / (2 * a)
-                res = [[], [[r, -i], [r, i]]]
+                r, i = -b / a / 2, e / a / 2
+                out[1].extend([complex(r, -i), complex(r, i)])
             else:
-                if b < 0: res = [[(2 * c) / (-b + e), (-b + e) / (2 * a)], []]
-                else: res = [[(-b - e) / (2 * a), (2 * c) / (-b - e)], []]
+                if b < 0: out[0].extend([c / (-b + e) * 2, (-b + e) / a / 2])
+                else: out[0].extend([-(b + e) / a / 2, -c / (b + e) * 2])
         else:
-            odd = p.deg() % 2
-             # Bairstow's method is ill-behaved on odd-degree polynomials; "salt" with a factor of x - 1 and catch later
-            if odd: p *= polynomial((-1, 1))
-            res = [[], []]
+            odd = p.deg() % 2 # Bairstow's method is ill-behaved on odd-degree polynomials
+            if odd: p *= polyn(-1, 1) # ∴ salt with x - 1 and catch later
             while p.deg() > 2:
-                u, v = self[-2] / self[-1], self[-3] / self[-1]
-                x, y, bairprec = D(3), D(4), D(prec * prec * prec)
+                u, v, x, y = p[-2] / p[-1], p[-3] / p[-1], None, None
+                while x == None and y == None:
+                    try: x, y = p.bairstowstep(u, v)
+                    except ZeroDivisionError: u, v = u + 0.1, v + 0.1
                 for q in range(256):
-                    if not isclose((x * x + y * y).sqrt(), zero, abs_tol=bairprec):
+                    if not isclose(hypot(x, y), 0, abs_tol=1e-15):
                         x, y = p.bairstowstep(u, v)
                         u, v = u + x, v + y
                     else: break
                 p = p.quadruffini(u, v)[0]
-                load = polynomial((v, u, 1)).roots()
-                res[0].extend(load[0])
-                res[1].extend(load[1])
+                new = polyn(v, u, 1).roots()
+                out[0].extend(new[0])
+                out[1].extend(new[1])
             last = p.roots()
-            res[0].extend(last[0])
-            res[1].extend(last[1])
+            out[0].extend(last[0])
+            out[1].extend(last[1])
             if odd:
-                notr = True
-                for r in res[0]:
-                    if isclose(r, D(1), abs_tol=prec):
-                        res[0].remove(r)
-                        notr = False
+                for i in range(len(out[0])):
+                    if isclose(out[0][i], 1, abs_tol=1e-15):
+                        out[0].pop(i)
                         break
-                if notr:
-                    for c in res[1]:
-                        if isclose(c[0], D(1), abs_tol=prec):
-                            res[1].remove(c)
-                            break
-        # At the end the previously found number of "trivial" zeros are appended to res.
-        res[0].extend([zero for i in range(nz)])
-        return res
+        return out
 
 def polynomroot(coeffs, precdigits = 15):
     """Finds all the roots of polynomial(coeffs) to [precdigits] decimal places.
